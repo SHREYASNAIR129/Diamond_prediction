3b53bebcbdcaaf608beefe94e8caee4c9c55a92c
SHREYASNAIR129 <nairsm_1@rknec.edu>
2023-01-19 18:40:14 +0530

    pip to pip3

--- dataLoading.py
+++ dataLoading.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-os.system("pip install -r requirements.txt")
+os.system("pip3 install -r requirements.txt")
 
 def load_data():
     data = pd.read_csv("./diamonds.csv")
